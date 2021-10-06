8ea8a72cf46362ff2d2d840b2aa053a59fcab86d
bernardow <bernardoritzel@gmail.com>
2021-10-06 18:03:44 -0300

    In Level Doors Working

---
+++ PixelMoon/Assets/Scripts/Interactable/LVLDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LVLDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform outPutDoor;
+    [SerializeField] private Fade gm;
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            player.transform.position = OtherDoorPos(outPutDoor);
+            gm.RunFade(true);
+            
+        }
+    }
+
+    private Vector3 OtherDoorPos(Transform outPut)
+    {
+        var newOutput = outPut.position;
+        return newOutput;
+    }
+}
