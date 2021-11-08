efa2d3cf770188fcb49d67ba0b31276ebd8ea136
bernardow <bernardoritzel@gmail.com>
2021-11-08 10:27:01 -0300

    Fixed_Table

--- PixelMoon/Assets/Scripts/Interactable/Table.cs
+++ PixelMoon/Assets/Scripts/Interactable/Table.cs
@@ -8,6 +8,7 @@ public class Table : MonoBehaviour
     [SerializeField] private Transform armEdge;
     [SerializeField] private GameObject lamp = null;
     [SerializeField] private Transform restPoint;
+    private bool canCatch = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,20 +19,28 @@ public class Table : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (canCatch)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                pl.InventoryCheck().Remove("lamp");
+                lamp.GetComponent<Transform>().position = restPoint.position;
+            }
+            if (Input.GetKeyDown(KeyCode.E) && lamp.GetComponent<Transform>().position == restPoint.position)
+            {
+                pl.InventoryCheck().Add("lamp");
+                lamp.GetComponent<Transform>().position = armEdge.position;
+            }
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            pl.InventoryCheck().Remove("lamp");
-            lamp.GetComponent<Transform>().position = restPoint.position;
-        }
-        if(Input.GetKeyDown(KeyCode.E) && lamp.GetComponent<Transform>().position == restPoint.position)
-        {
-            pl.InventoryCheck().Add("lamp");
-            lamp.GetComponent<Transform>().position = armEdge.position;
-        }
+        canCatch = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canCatch = false;
     }
 }
