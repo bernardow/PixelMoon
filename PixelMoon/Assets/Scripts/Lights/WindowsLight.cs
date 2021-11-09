65e1e228309e6d2ad6632cf7bee24f5827426786
bernardow <bernardoritzel@gmail.com>
2021-11-09 09:43:40 -0300

    Window-Light_Added

---
+++ PixelMoon/Assets/Scripts/Lights/WindowsLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsLight : MonoBehaviour
+{
+    private Lamp lp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lp = FindObjectOfType<Lamp>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            lp.inLight = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            lp.inLight = false;
+    }
+}
