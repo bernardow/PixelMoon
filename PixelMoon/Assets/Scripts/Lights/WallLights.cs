a6a10244aa74fdbfe471feb57f67a81250910bd1
bernardow <bernardoritzel@gmail.com>
2021-11-09 10:28:28 -0300

    Wall-Light-Added

---
+++ PixelMoon/Assets/Scripts/Lights/WallLights.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallLights : MonoBehaviour
+{
+    [SerializeField] private GameObject lightFrame = null;
+    private Lamp lp = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lp = FindObjectOfType<Lamp>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(lp.inLight && collision.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            lightFrame.SetActive(true);
+        }
+    }
+}
--- PixelMoon/Assets/Scripts/Lights/WindowsLight.cs
+++ PixelMoon/Assets/Scripts/Lights/WindowsLight.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WindowsLight : MonoBehaviour
 {
-    private Lamp lp;
+    private Lamp lp = null;
 
     // Start is called before the first frame update
     void Start()
