f9b64a9111e01c5107085e1c3b57304c48903449
bernardow <bernardoritzel@gmail.com>
2021-11-11 15:56:03 -0300

    Squashed commit of the following:
    commit 82b10edb6839bc75b40c9c7df891422658d5a151
    Merge: f305a39 7581460
    Author: bernardow <bernardoritzel@gmail.com>
    Date:   Thu Nov 11 14:31:19 2021 -0300
    
        Merge branch 'main' of https://github.com/bernardow/PixelMoon
    
    commit f305a3909f4d701a20bf545b7823e9e1b2b5fa3a
    Author: bernardow <bernardoritzel@gmail.com>
    Date:   Thu Nov 11 14:30:43 2021 -0300
    
        Dayly-Commit

---
+++ PixelMoon/Assets/Scripts/Traps/FallingGround.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGround : MonoBehaviour
+{
+    private bool initiateTimer = false;
+    private float tSLTouch = 0f;
+    private Rigidbody2D rb;
+    [SerializeField] private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (initiateTimer)
+            tSLTouch += Time.deltaTime;
+
+        if(tSLTouch >= timer)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        initiateTimer = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        initiateTimer = false;
+        tSLTouch = 0f;
+    }
+}
